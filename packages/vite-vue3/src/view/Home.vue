c1e74d45a68b5c76d8fc3e98c8949008425b9fb4
int.liuwenbo <1090851297@qq.com>
2024-03-25 16:41:09 +0800

    :sparkles: feat: logo添加

--- packages/vite-vue3/src/view/Home.vue
+++ packages/vite-vue3/src/view/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="flex h-full w-full flex-col">
     <header
-      class="flex h-14 w-full items-center justify-between bg-sky-400 bg-gradient-to-r from-cyan-500 to-blue-500 px-4 text-white shadow-xl"
+      class="flex h-14 w-full items-center justify-between bg-gradient-to-r from-violet-500 to-cyan-500 px-4 text-white shadow-xl"
     >
-      <div class="logo-container">full-featured-vite</div>
+      <div class="flex items-center">
+        <img class="h-16 w-16" src="assets/images/logo.png" alt="" />
+        <span class="text-xl"></span>
+      </div>
       <div class="flex items-center">
         <div class="mr-5">北京</div>
         <span class="mx-1 rounded-md bg-purple-500/75 p-1 text-purple-50">{{
@@ -19,7 +22,10 @@
         }}</span>
       </div>
     </header>
-    <section>123</section>
+    <section class="flex h-auto w-full flex-col items-center justify-center">
+      <img class="h-96 w-96" src="assets/images/logo.png" alt="" />
+      <h1 class="text text-5xl">full-featured</h1>
+    </section>
     <footer></footer>
   </div>
 </template>
