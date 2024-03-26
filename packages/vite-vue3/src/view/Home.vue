5b4a1fde0590bd5e53dbd903ae09e6317319f39e
int.liuwenbo <1090851297@qq.com>
2024-03-26 14:23:11 +0800

    :sparkles: feat: ui完善

--- packages/vite-vue3/src/view/Home.vue
+++ packages/vite-vue3/src/view/Home.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="flex h-full w-full flex-col">
     <header
-      class="flex h-14 w-full items-center justify-between bg-gradient-to-r from-violet-500 to-cyan-500 px-4 text-white shadow-xl"
+      class="flex h-14 w-full items-center justify-between px-4 text-white"
     >
       <div class="flex items-center">
         <img class="h-16 w-16" src="assets/images/logo.png" alt="" />
         <span class="text-xl"></span>
       </div>
-      <div class="flex items-center">
+      <div class="flex items-center text-black">
         <div class="mr-5">北京</div>
-        <span class="mx-1 rounded-md bg-purple-500/75 p-1 text-purple-50">{{
+        <span class="mx-1 rounded-md bg-sky-500/75 p-1 text-purple-50">{{
           hour
         }}</span>
         :
-        <span class="mx-1 rounded-md bg-purple-500/75 p-1 text-purple-50">{{
+        <span class="mx-1 rounded-md bg-sky-500/75 p-1 text-purple-50">{{
           min
         }}</span>
         :
-        <span class="mx-1 rounded-md bg-purple-500/75 p-1 text-purple-50">{{
+        <span class="mx-1 rounded-md bg-sky-500/75 p-1 text-purple-50">{{
           second
         }}</span>
       </div>
     </header>
     <section class="flex h-auto w-full flex-col items-center justify-center">
       <img class="h-96 w-96" src="assets/images/logo.png" alt="" />
-      <h1 class="text text-5xl">full-featured</h1>
+      <div class="flex">
+        <div
+          class="mr-10 rounded-md bg-sky-500 px-5 py-2 text-white transition-all hover:mr-4"
+        >
+          文档
+        </div>
+        <div
+          class="hover:bg-gradient-r-to rounded-md bg-sky-500 px-5 py-2 text-white transition-all"
+        >
+          Github
+        </div>
+      </div>
     </section>
     <footer></footer>
   </div>
