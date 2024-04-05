4f0b84dd736bcc8afc7fe4eea780ac837abb5d86
bobi <1090851297@qq.com>
2024-04-05 11:09:15 +0800

    :sparkles: feat: 模版文件cli创建complete

--- packages/cli/src/create.js
+++ packages/cli/src/create.js
@@ -4,12 +4,12 @@ const fs = require("fs-extra");
 const path = require("path");
 const argv = require("minimist")(process.argv.slice(2));
 // 🌸 生成模版文件
-const init = async function () {
-  const cwd = process.cwd();
+module.exports = async function () {
+  const root = process.cwd();
 
   // 🌸 获取目标路径
   const targetDir = argv._[0] ? argv._[0] : "./";
-  const targetPath = path.join(cwd, targetDir);
+  const targetPath = path.join(root, targetDir);
   // 🌸 获取目标模版
   const tempDir = path.join(__dirname, `../${argv.temp}`);
   // 🌸 确保目标文件夹存在,也就是说如果不存在就创建一个
@@ -17,34 +17,21 @@ const init = async function () {
   fs.readdir(targetPath, (err, files) => {
     if (files.length > 0) {
       console.log("\n📁 Such directory is not empty!\n");
-      throw Error("Such directory is not empty!");
       return;
     }
   });
 
   // 🌸 获取
-  const writeFile = (file, content) => {
+  const writeFile = (file) => {
     const targetTempPath = path.join(tempDir, file);
     const targetFilePath = path.join(targetDir, file);
-    if (content) {
-      fs.writeFile(targetFilePath, content);
-    } else {
-      fs.copy(targetTempPath, targetFilePath);
-    }
+    fs.copy(targetTempPath, targetFilePath);
   };
   let fileArr = await fs.readdir(tempDir);
   for (let file of fileArr.filter((item) => item !== "package.json")) {
     writeFile(file);
   }
-  // 🌸 获取package.json
-  const pkg = require(path.join(tempDir, "package.json"));
-  pkg.name = path.basename(targetPath);
-  writeFile("package.json", JSON.stringify(pkg, null, 2));
-  if (targetDir !== cwd) {
-    console.log(`\n ⬆️  cd ${path.relative(cwd, targetDir)} \n`);
-  }
-  console.log(`\n ⬇️  npm install \n`);
-  console.log(`\n ⬇️  npm run dev \n`);
+  // 🌸 获取
 };
 init().catch((err) => {
   console.log("");
