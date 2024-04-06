d591577979f7472a65ef1360f617baa14a5db9cf
bobi <1090851297@qq.com>
2024-04-06 14:20:13 +0800

    :recycle: refactor: directory change

--- packages/vite-vue3/src/types/components.ts
+++
@@ -1,8 +0,0 @@
-// 🌸 messageBox导出方法
-export interface useMessageType {
-  info?: (content: string) => void
-  warning?: (content: string) => void
-  error?: (content: string) => void
-  success?: (content: string) => void
-}
-export type useMessageFunType = () => void
