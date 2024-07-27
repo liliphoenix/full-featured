b3dbe529ae8334071be6a6d7be319699c32c0b98
bobi <1090851297@qq.com>
2024-07-27 20:19:37 +0800

    :sparkles: feat: webpack-react

--- packages/cli/template/webpack5-React18/src/App.tsx
+++ packages/cli/template/webpack5-React18/src/App.tsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './App.less'
 import './tailwind.css'
 import './test.less'
 // 图片不像img那样内嵌式的引入， 要用这种方式引入
 import JPG from './assets/1.jpg'
 function App() {
-  console.log(process.env.NODE_ENV)
-
+  const previewDom = useRef(null)
+  const preview = (e: any) => {
+    const file = e.target.files[0]
+    const dom = previewDom.current
+    const fileReader = new FileReader()
+    if (file.type === 'image/jpeg' && dom) {
+      fileReader.onload = (event) => {
+        const res = event.target?.result
+        dom.src = res
+      }
+      fileReader.readAsDataURL(file)
+    }
+    send(file)
+  }
+  const send = (file: any) => {
+    const formData = new FormData()
+    formData.append('file', file)
+  }
   return (
     <>
       <div className="">Hello Full-Featured!</div>
       <div className="text-2xl">tailwind-css</div>
       <img className="w-20" src={JPG} alt="" />
-      <div className="purgecss">1234556</div>
+      <input type="file" onChange={preview} />
+      <div className="preview">
+        <img className="w-20" ref={previewDom} src="" alt="" />
+      </div>
     </>
   )
 }
